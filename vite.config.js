860ccb272c88a8d663c1c192d1d7dae3a3f09557
abdoelkasby58 <abdoelkasby58@example.com>
2026-05-03 17:37:08 +0300

    base remove

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/slimedonew",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
